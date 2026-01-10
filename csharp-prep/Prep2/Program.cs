534a146d3a96dc20519f86a12c4d781d86318140
Mia Smith <mcpmies13@gmail.com>
2026-01-10 10:56:00 -0700

    Assignment 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Please input your grade percentage: ");
+        string gradeString = Console.ReadLine();
+        int grade = int.Parse(gradeString);
+
+        string letterGrade = "";
+
+        if (grade >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (grade >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (grade >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (grade >= 60)
+        {
+            letterGrade = "D";
+        }
+        else
+        {
+            letterGrade = "F";
+        }
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations you passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, you did not pass. Keep trying!");
+        }
+
+        Console.WriteLine($"Your grade is a(n) {letterGrade}");
     }
 }
