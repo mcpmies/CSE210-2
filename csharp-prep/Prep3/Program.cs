27de5206bc209a9e0a57b2d237c5cd07ea9fb116
Mia Smith <mcpmies13@gmail.com>
2026-01-13 15:21:55 -0700

    C# Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1,100);
+
+        int guess;
+
+        do
+        {
+            Console.Write("What is your guess?  ");
+            string input2 = Console.ReadLine();
+            guess = int.Parse(input2);
+
+            if (guess == magicNumber)
+            {
+                Console.WriteLine("You guessed it!");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+
+        } while (guess != magicNumber);
+
     }
 }
