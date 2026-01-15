40a2b0d11455c680c7e9d7552268da147f88fd15
Mia Smith <mcpmies13@gmail.com>
2026-01-15 09:19:22 -0700

    C# Prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,38 @@
 using System;
+using System.Runtime.InteropServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int input;
+        int zero = 0;
+        int sum = 0;
+        int greatest = 0;
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        do
+        {
+            Console.Write("Enter number:  ");
+            input = int.Parse(Console.ReadLine());
+            numbers.Add(input);
+        } while (input != 0);
+
+        numbers.Remove(zero);
+
+        foreach (int num in numbers)
+        {
+           sum += num;
+           if (num > greatest)
+            {
+                greatest = num;
+            }
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {sum/numbers.Count}");
+        Console.WriteLine($"The largest number is {greatest}");
     }
 }
